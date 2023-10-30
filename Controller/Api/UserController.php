6cc770219b3ac621fc76bb98d948e6aee44faf99
COMPRyanLI <rli03@wesleyan.edu>
2023-10-30 00:25:24 -0400

    rest api update

--- Controller/Api/UserController.php
+++ Controller/Api/UserController.php
@@ -11,64 +11,103 @@ class UserController extends BaseController
    
         if (strtoupper($requestMethod) == 'POST'){
             $postData = json_decode(file_get_contents('php://input'),true);
+            $username = $postData['username'];
+            $password = $postData['password'];
             // Instantiate a UserModel to create a new user
             $userModel =  new UserModel();
-            $userModel -> createUser($postData);
+            $userModel -> createUser($username,$password);
 
         }
        
 }
      public function checkAction(){
         $requestMethod = $_SERVER["REQUEST_METHOD"];
-        if (strtoupper($requestMethod) == 'GET'){
+        if (strtoupper($requestMethod) == 'POST'){
             $postData = json_decode(file_get_contents('php://input'),true);
+            $username = $postData['username'];
+            $password = $postData['password'];
             // Instantiate a UserModel to check login
             $userModel =  new UserModel();
-            $userModel -> checkUser($postData);
+            $userModel -> checkUser($username,$password);
         } 
 
      }
 
      public function deleteAction(){
         $requestMethod = $_SERVER["REQUEST_METHOD"];
-        if (strtoupper($requestMethod) == 'DELETE'){
+        if (strtoupper($requestMethod) == 'POST'){
             $postData = json_decode(file_get_contents('php://input'),true);
+            $id = $postData['id'];
             // Instantiate a UserModel to delete a rating
             $userModel =  new UserModel();
-            $userModel -> deleteRating($postData);
+            $userModel -> deleteRating($id);
         } 
 
      }
      public function updateAction(){
         $requestMethod = $_SERVER["REQUEST_METHOD"];
-        if (strtoupper($requestMethod) == 'PUT'){
+        if (strtoupper($requestMethod) == 'POST'){
             $postData = json_decode(file_get_contents('php://input'),true);
+            $id = $postData['id'];
+            $song = $postData['song'];
+            $rating = $postData['rating'];
+            $artist = $postData['artist'];
             // Instantiate a UserModel to update a rating
             $userModel =  new UserModel();
-            $userModel -> updateRating($postData);
+            $userModel -> updateRating($artist,$song,$rating,$id);
      }
 }
      public function addAction(){
         $requestMethod = $_SERVER["REQUEST_METHOD"];
         if (strtoupper($requestMethod) == 'POST'){
             $postData = json_decode(file_get_contents('php://input'),true);
+            $username = $postData['username'];
+            $artist = $postData['artist'];
+            $song = $postData['song'];
+            $rating = $postData['rating'];
             // Instantiate a UserModel to create a new rating
             $userModel =  new UserModel();
-            $userModel -> addRating($postData);
+            $userModel -> addRating($username,$artist,$song,$rating);
         } 
 
 
      }
      public function viewAction(){
-        $requestMethod = $_SERVER["REQUEST_METHOD"];
-        if (strtoupper($requestMethod) == 'GET'){
-            $postData = json_decode(file_get_contents('php://input'),true);
-            // Instantiate a UserModel to create a new rating
-            $userModel =  new UserModel();
-            $userModel -> getRating($postData);
-        } 
-
+        $strErrorDesc = '';
+         $requestMethod = $_SERVER["REQUEST_METHOD"];
+         $arrQueryStringParams = $this->getQueryStringParams();
+         if (strtoupper($requestMethod) == 'GET') {
+             try {
+                 $userModel = new UserModel();
+                 $intLimit = 20;
+                 if (isset($arrQueryStringParams['limit']) && $arrQueryStringParams['limit']) {
+                     $intLimit = $arrQueryStringParams['limit'];
+                 }
+                 $arrUsers = $userModel->getRating($intLimit);
+                 $responseData = json_encode($arrUsers);
+             } catch (Error $e) {
+                 $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                 $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+             }
+         } else {
+             $strErrorDesc = 'Method not supported';
+             $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+         }
+         // send output 
+         if (!$strErrorDesc) {
+             $this->sendOutput(
+                 $responseData,
+                 array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+             );
+         } else {
+             $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
+                 array('Content-Type: application/json', $strErrorHeader)
+             );
+         }
      }
 
-}
+    }
+   
+
+
 ?>
--- Model/UserModel.php
+++ Model/UserModel.php
@@ -2,25 +2,20 @@
 require_once PROJECT_ROOT_PATH . "/Model/Database.php";
 class UserModel extends Database
 {   
-    public function createUser($userData){ // registration function model
+    public function createUser($username,$password){ // registration function model
         $sql = "INSERT INTO users (username, password) VALUES(?,?) ";
-        $hashed_password= password_hash($userData['password'],  PASSWORD_DEFAULT); 
-        executeStatement($sql, ["ss", $userData['username'], $hashedPassword]);
+        $hashed_password= password_hash($password,  PASSWORD_DEFAULT); 
+        $stmt = $this->connection->prepare($sql);
+        $stmt->bind_param("ss", $username, $hashedPassword);
+        $stmt->execute();  
     }
 
-    public function checkUser($userData){ // user model function for login.php
-        $user = $userData['username'];
-        $password = $userData['password'];
-        $result = select("SELECT * FROM users WHERE username = ?", ["s", $user]);
+    public function checkUser($username,$password){ // user model function for login.php
+        $result = select("SELECT * FROM users WHERE username = ?", ["s", $username]);
         $num = mysqli_num_rows($result);
         $row = mysqli_fetch_assoc($result);
         $hashed_password = $row["password"];
-
        if ($num > 0 && password_verify($password, $hashed_password)) {
-        session_start();
-        echo('PHPSESSID: ' . session_id($_GET['session_id']));
-        $_SESSION['name']   = $user;
-        $_SESSION["loggedin"] = true;
         return true;
     }
     else{
@@ -29,30 +24,28 @@ class UserModel extends Database
 
 }
     
-    public function updateRating($userData){ // user model function for update
+    public function updateRating($id,$artist,$song,$rating){ // user model function for update
         $sql = "UPDATE ratings SET artist = ?, song = ?, rating = ? WHERE id = ?" ;
-        executeStatement( $sql , ["ssii",$userData]);
+        $stmt = $this->connection->prepare($sql);
+        $stmt->bind_param("ssii",$artist,$song,$rating,$id);
+        $stmt->execute();        
 
     }
     
-    public function deleteRating($userData){ // user model function for delete
-        $sql_query = "SELECT song, artist, rating FROM ratings WHERE id = ?";
-        $id = $userData['id'];
-        $song = $userData['song'];
-        $result = select($sql_query, ["i", $id]);
-        $num = mysqli_num_rows($result);
-        if ($num === 0){
-            $sql = "DELETE FROM ratings WHERE song = ? AND id = ?";
-            executeStatement( $sql , ["si",$song,$id]);
-    }
+    public function deleteRating($id){ // user model function for delete
+        $sql = "DELETE FROM ratings WHERE id = ?";
+        $stmt = $this->connection->prepare($sql);
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+
+
+
+    
 }
 
     public function addRating($userData){ // user model function for addsong
         $sql1 = "SELECT * FROM ratings WHERE song = ? ";
-        $song = $userData['song'];
-        $username = $userData['username'];
-        $rating = $userData['rating'];
-        $artist = $userData['artist'];
+       
         $result = select($sql1, ["s",$song]);
         $num = mysqli_num_rows($result);
         if ($num === 0){
@@ -62,10 +55,9 @@ class UserModel extends Database
 
     }
 
-    public function getRating($userData) // user model function for view
+    public function getRating($limit) // user model function for view
     {
-        $id = $userData['id'];
-        return $this->select("SELECT * FROM ratings WHERE id = ?", ["i", $id]);
+        return $this->select("SELECT * FROM ratings ORDER BY id ASC LIMIT ?", ["i", $limit]);
     }
 }
 ?>
