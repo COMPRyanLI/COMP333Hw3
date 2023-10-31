86863e65086d5160f6feabe8b4514f14bd784681
COMPRyanLI <rli03@wesleyan.edu>
2023-10-31 13:50:32 -0400

    change some properties

--- edit.js
+++ edit.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 
-function UpdateSong({ song, onEditSong }) {
-  const [rating, setRating] = useState(song.rating);
+function UpdateSong({ song: initialSong, onEditSong }) {
+  const [song, setTitle] = useState(initialSong.song);
+  const [artist, setArtist] = useState(initialSong.artist);
+  const [rating, setRating] = useState(initialSong.rating);
 
   const handleEditSong = () => {
-    
-    // Update the song object with the new rating
-    const updatedSong = { ...song, rating: parseInt(rating) };
+    // Update the song object with the new properties
+    const updatedSong = { ...initialSong, song, artist, rating: parseInt(rating) };
 
     // Call the parent component's callback function to edit the song
     onEditSong(updatedSong);
@@ -14,17 +15,25 @@ function UpdateSong({ song, onEditSong }) {
 
   return (
     <div>
-      <h2>Edit Song Rating</h2>
+      <h2>Edit Song</h2>
       <p>
-        Artist: {song.artist}, Song Title: {song.title}, Current Rating: {song.rating}
+        Artist: {initialSong.artist}, Song Title: {initialSong.song}, Current Rating: {initialSong.rating}
       </p>
-      <form>
+      <form onSubmit={e => e.preventDefault()}> {/* Prevent default form submission */}
         <div>
+          <label>New Title:</label>
+          <input type="text" value={song} onChange={(e) => setTitle(e.target.value)} /> 
+          <br />
+          
+          <label>New Artist:</label>
+          <input type="text" value={artist} onChange={(e) => setArtist(e.target.value)} />
+          <br/>
           <label>New Rating:</label>
           <input type="number" value={rating} onChange={(e) => setRating(e.target.value)} />
+          <br />
         </div>
         <button type="button" onClick={handleEditSong}>
-          Update Rating
+          Update Song
         </button>
       </form>
     </div>
