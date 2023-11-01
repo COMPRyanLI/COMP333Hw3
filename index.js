e5c04faec65b92f80632c0e440f9ce5a0070ac17
lgartrell11 <gartrell.lance1@gmail.com>
2023-11-01 03:31:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+<<<<<<< HEAD
 // Renders app.js
+=======
+>>>>>>> 6-creatingediting-ratings-frontend
 ReactDOM.render(
   <React.StrictMode>
     <App />
